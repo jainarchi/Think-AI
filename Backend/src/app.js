f2cd38b823f75c345503ed40273fdc5b08ed74f7
jainarchi <imarchijain2405@gmail.com>
2026-03-31 23:37:48 +0530

    setup: initialize backend project

---
+++ Backend/src/app.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import authRouter from './routes/auth.routes.js'
+import chatRouter from './routes/chat.routes.js'
+import { errorHandler } from './utils/errorHandler.js'
+import morgan from 'morgan'
+import cors from "cors"
+
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan("dev"))
+
+app.use(cors({
+    origin : 'http://localhost:5173' ,
+    credentials:true,
+    methods:["POST" , "GET" , "PUT" , "DELETE" , "PATCH"]
+}))
+
+
+
+
+
+
+app.use('/api/auth' , authRouter)
+app.use('/api/chats' , chatRouter)
+
+
+
+
+
+app.use(errorHandler)
+
+
+
+export default app
