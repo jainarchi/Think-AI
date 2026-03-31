337f787fd3e6d98060c18acf985d7b7911630adf
jainarchi <imarchijain2405@gmail.com>
2026-04-01 01:42:05 +0530

    added UI layer dashboard and make shared components , make full responsive UI and also maintain a better UX

---
+++ Frontend/src/features/chat/components/InputBar.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useRef } from "react";
+import { RiArrowUpLine, RiAddLine } from "@remixicon/react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useChat } from "../hook/useChat";
+import { useSavePrompt } from "../hook/useSavePrompt";
+import "../style/inputBar.scss";
+
+const InputBar = () => {
+  const inpRef = useRef(null);
+  const { handleSendMessage } = useChat();
+  const { handleClearSelectPrompt, handleSavePrompt } = useSavePrompt();
+
+  const currentChatId = useSelector((state) => state.chat.currentChatId);
+  const selectPrompt = useSelector((state) => state.savePrompt.selectPrompt);
+
+  const [chatInput, setchatInput] = useState("");
+  const [showInputOptions, setShowInputOptions] = useState(false);
+
+  useEffect(() => {
+    if (selectPrompt) {
+      setchatInput(selectPrompt); // Input fill
+      handleClearSelectPrompt(); //clear Redux state & chatInput free to modify
+    }
+  }, [selectPrompt, handleClearSelectPrompt]);
+
+  const handleSubmitMessage = (e) => {
+    e.preventDefault();
+    console.log(chatInput);
+
+    const trimmedMessage = chatInput.trim();
+    if (!trimmedMessage) {
+      return;
+    }
+
+    handleSendMessage({
+      message: trimmedMessage,
+      chatId: currentChatId,
+    });
+
+    setchatInput("");
+  };
+
+  const handleChange = (e) => {
+    setchatInput(e.target.value);
+
+    const inp = inpRef.current;
+    if(!inp) return 
+
+    inp.style.height = "auto"; // reset
+    inp.style.height = inp.scrollHeight + "px"; // grow
+
+  };
+
+  const inputOptionToggle = () => {
+    setShowInputOptions(!showInputOptions);
+  };
+
+  const savePrompt = () => {
+    // btn disable
+
+    if (chatInput.trim().length > 0) {
+      handleSavePrompt(chatInput);
+    }
+
+    setShowInputOptions(false);
+  };
+
+  return (
+    <div className="inputBar">
+      <form className="input-box" onSubmit={handleSubmitMessage}>
+        <div className={`inputOption ${showInputOptions && "show"}`}>
+          <div onClick={savePrompt}>save prompt</div>
+        </div>
+
+        <div className="sendIcon" onClick={inputOptionToggle}>
+          <RiAddLine size={"1.4rem"} />
+        </div>
+
+        <textarea
+          type="text"
+          className="input"
+          placeholder="Ask anything"
+          value={chatInput}
+          rows={1}
+          ref={inpRef}
+          onChange={handleChange}
+       
+        />
+
+        <button className="sendIcon" disabled={!chatInput.trim()}>
+          <RiArrowUpLine size={"1.5rem"} />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default InputBar;
---
+++ Frontend/src/features/chat/pages/Dashboard.jsx
@@ -0,0 +1,130 @@
+import { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { useChat } from "../hook/useChat";
+import "../style/dashboard.scss";
+
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import remarkGfm from "remark-gfm";
+
+import InputBar from "../components/InputBar";
+
+const Dashboard = () => {
+  const chat = useChat();
+
+  const chats = useSelector((state) => state.chat.chats);
+  const currentChatId = useSelector((state) => state.chat.currentChatId);
+  const lastMessageRef = useRef(null);
+  const isLoading = useSelector((state) => state.chat.isLoading);
+
+  const [showThinking, setShowThinking] = useState(false);
+
+  useEffect(() => {
+    let timer;
+
+    if (isLoading) {
+      timer = setTimeout(() => {
+        setShowThinking(true);
+      }, 700);
+    } else {
+      setShowThinking(false);
+    }
+
+    return () => clearTimeout(timer);
+  }, [isLoading]);
+
+  useEffect(() => {
+    chat.initializationSocketConnection();
+  }, []);
+
+  useEffect(() => {
+    if (lastMessageRef.current) {
+      lastMessageRef.current.focus();
+      lastMessageRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [chats[currentChatId]?.messages]);
+
+  return (
+    <>
+      <div className="dashboard">
+        <div className="messages">
+          {/* {console.log(chats[currentChatId])} */}
+
+          {currentChatId === null ? (
+            <>
+              <h1 className="mainHeading">What can I help you today ?</h1>
+            </>
+          ) : (
+            chats[currentChatId]?.messages.map((message, index, arr) => {
+              const isLast = index === arr.length - 1;
+
+              return (
+                <div
+                  key={message.id || index}
+                  ref={isLast ? lastMessageRef : null}
+                  tabIndex={-1}
+                  className={`message ${message.role === "user" ? "user" : "assistant"}`}
+                >
+                  {message.role === "user" ? (
+                    <p className="text">{message.content}</p>
+                  ) : (
+                    <ReactMarkdown
+                      components={{
+                        p: ({ children }) => (
+                          <p className="md-paragraph">{children}</p>
+                        ),
+                        ul: ({ children }) => (
+                          <ul className="md-list">{children}</ul>
+                        ),
+                        ol: ({ children }) => (
+                          <ol className="md-olist">{children}</ol>
+                        ),
+
+                        code({ inline, className, children, ...props }) {
+                          const match = /language-(\w+)/.exec(className || "");
+
+                          return !inline && match ? (
+                            <SyntaxHighlighter
+                              style={oneDark}
+                              language={match[1]}
+                              PreTag="div"
+                            >
+                              {String(children).replace(/\n$/, "")}
+                            </SyntaxHighlighter>
+                          ) : (
+                            <code className="md-code" {...props}>
+                              {children}
+                            </code>
+                          );
+                        },
+                      }}
+                      remarkPlugins={[remarkGfm]}
+                    >
+                      {message.content}
+                    </ReactMarkdown>
+                  )}
+                </div>
+              );
+            })
+          )}
+
+          {showThinking && (
+            <div className="message assistant">
+              <div className="thinking">
+                Thinking
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <InputBar />
+    </>
+  );
+};
+
+export default Dashboard;
