5dbc30e48b72e6be988357bfcca9fac08a7d06a9
jainarchi <imarchijain2405@gmail.com>
2026-04-02 02:56:25 +0530

    added url

--- Frontend/src/features/chat/service/savePrompt.api.js
+++ Frontend/src/features/chat/service/savePrompt.api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
+
 
 const api = axios.create({
     baseURL:BASE_URL,
